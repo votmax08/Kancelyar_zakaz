da4b94621cecb8ade14324c055ad611b483bb167
votmax08 <votmax08@ya.ru>
2018-10-17 23:31:16 +0300

    Логика интерфейса
    Считывание массива с Excel файла. Вывод всего этого на форму. Добавление экземпляра в таблицу заказа.

--- Kancelyar_zakaz/Form1.Designer.cs
+++ Kancelyar_zakaz/Form1.Designer.cs
@@ -34,13 +34,22 @@
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Артикул = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,7 +61,8 @@
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2,
-            this.Column3});
+            this.Column3,
+            this.Артикул});
             this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaption;
             this.dataGridView1.Location = new System.Drawing.Point(376, 63);
             this.dataGridView1.Name = "dataGridView1";
@@ -89,33 +99,42 @@
             // 
             // comboBox1
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 95);
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Location = new System.Drawing.Point(12, 76);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(154, 21);
+            this.comboBox1.Sorted = true;
             this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // statusStrip1
             // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
             this.statusStrip1.Location = new System.Drawing.Point(0, 428);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Size = new System.Drawing.Size(800, 22);
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
             // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 76);
+            this.label1.Location = new System.Drawing.Point(13, 57);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.Size = new System.Drawing.Size(95, 13);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Выбор";
+            this.label1.Text = "Выбор категории";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 125);
+            this.label2.Location = new System.Drawing.Point(13, 152);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(66, 13);
             this.label2.TabIndex = 3;
@@ -123,7 +142,8 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(123, 123);
+            this.numericUpDown1.Enabled = false;
+            this.numericUpDown1.Location = new System.Drawing.Point(123, 150);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             99,
             0,
@@ -146,7 +166,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(91, 149);
+            this.button1.Location = new System.Drawing.Point(91, 247);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 5;
@@ -175,24 +195,97 @@
             this.button3.Text = "Сделать заказ";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(172, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "eдиницы";
+            this.label3.Visible = false;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.Enabled = false;
+            this.comboBox2.Location = new System.Drawing.Point(12, 123);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(154, 21);
+            this.comboBox2.Sorted = true;
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Выбор товара";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(16, 196);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(77, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Изменить";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Артикул с сайта Komus";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(15, 214);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // Артикул
+            // 
+            this.Артикул.HeaderText = "Column4";
+            this.Артикул.Name = "Артикул";
+            this.Артикул.ReadOnly = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Заказ оборудования";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -213,6 +306,14 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Артикул;
     }
 }
 
--- Kancelyar_zakaz/Form1.cs
+++ Kancelyar_zakaz/Form1.cs
@@ -8,12 +8,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
-using System.Xml.Serialization;
+using Microsoft.Office.Interop.Excel;
+using Excel =  Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using Word = Microsoft.Office.Interop.Word;
+using System.Reflection;
+
 
 namespace Kancelyar_zakaz
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
@@ -21,27 +27,133 @@ namespace Kancelyar_zakaz
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            if (dataGridView1.Rows.Count > 0)
-                if (dataGridView1.SelectedCells.Count > 0)
-                    button2.Visible = true;
+            
         }
 
+        List<Item> items = new List<Item>();
+        private static object[,] loadCellByCell(int row, int maxColNum, _Worksheet osheet)
+        {
+            var list = new object[2, maxColNum + 1];
+            for (int i = 1; i <= maxColNum; i++)
+            {
+                var RealExcelRangeLoc = osheet.Range[(object)osheet.Cells[row, i], (object)osheet.Cells[row, i]];
+                object valarrCheck;
+                try
+                {
+                    valarrCheck = RealExcelRangeLoc.Value[XlRangeValueDataType.xlRangeValueDefault];
+                }
+                catch
+                {
+                    valarrCheck = (object)RealExcelRangeLoc.Value2;
+                }
+                list[1, i] = valarrCheck;
+            }
+            return list;
+        }
+        
         private void Form1_Load(object sender, EventArgs e)
         {
-            dataGridView1.Width = 0;
-            foreach (DataGridViewColumn col in dataGridView1.Columns)
-                dataGridView1.Width += col.Width;
-            string path = @"Data.xml";
-            XmlSerializer formatter = new XmlSerializer(typeof(Predmet[]));
-            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+
+            Excel.Application ExcelObj = null;
+            _Workbook ecelbook = null;
+            try
             {
-                Predmet[] dePredmet = (Predmet[])formatter.Deserialize(fs);
-                for (int i = 0; i < dePredmet.Length; i++)
+                ExcelObj = new Excel.Application();
+                ExcelObj.DisplayAlerts = false;
+                const string f = @"\\Mac\Home\Desktop\Kancelyar_zakaz\Items.xlsx";
+                ecelbook = ExcelObj.Workbooks.Open(f, 0, true, 5, "", "", false, XlPlatform.xlWindows);
+                var sheets = ecelbook.Sheets;
+                var maxNumSheet = sheets.Count;
+                for (int i = 1; i <= maxNumSheet; i++)
                 {
-                    NewPredmet p = new NewPredmet(dePredmet[i].Id, dePredmet[i].Name, dePredmet[i].Art);
-                    comboBox1.Items.Add(p.Name);
-                    predmets.Add(p);
+                    var osheet = (_Worksheet)ecelbook.Sheets[i];
+                    Range excelRange = osheet.UsedRange;
+
+                    int maxColNum;
+                    int lastRow;
+                    try
+                    {
+                        maxColNum = excelRange.SpecialCells(XlCellType.xlCellTypeLastCell).Column;
+                        lastRow = excelRange.SpecialCells(XlCellType.xlCellTypeLastCell).Row;
+                    }
+                    catch
+                    {
+                        maxColNum = excelRange.Columns.Count;
+                        lastRow = excelRange.Rows.Count;
+                    }
+
+                    object[,] nameColArr = null;
+                    for (int l = 1; l <= lastRow; l++)
+                    {
+                        Range RealExcelRangeLoc = osheet.Range[(object)osheet.Cells[l, 1], (object)osheet.Cells[l, maxColNum]];
+                        object[,] valarr = null;
+                        try
+                        {
+                            var valarrCheck = RealExcelRangeLoc.Value[XlRangeValueDataType.xlRangeValueDefault];
+                            if (valarrCheck is object[,] || valarrCheck == null)
+                                valarr = (object[,])RealExcelRangeLoc.Value[XlRangeValueDataType.xlRangeValueDefault];
+                        }
+                        catch
+                        {
+                            valarr = loadCellByCell(l, maxColNum, osheet);
+                        }
+                        if (l == 1)
+                            nameColArr = valarr;
+                        else
+                        {
+                            Item tempItem = new Item();
+                            for (int j = 1; j <= nameColArr.Length; j++)
+                                switch (nameColArr[1, j].ToString())
+                                {
+                                    case "id":
+                                        tempItem.Id = Convert.ToInt32(valarr[1, j].ToString());
+                                        break;
+                                    case "name":
+                                        tempItem.Name = valarr[1, j].ToString();
+                                        break;
+                                    case "code":
+                                        if (valarr[1, j] != null)
+                                            tempItem.Code = valarr[1, j].ToString();
+                                        else
+                                            tempItem.Code = null;
+                                        break;
+                                    case "unit":
+                                        tempItem.Unit = valarr[1, j].ToString();
+                                        break;
+                                    case "category":
+                                        if (valarr[1, j] != null)
+                                            tempItem.Category = valarr[1, j].ToString();
+                                        else
+                                            tempItem.Category = "";
+                                        break;
+                                    case "komusart":
+                                        if (valarr[1, j] != null)
+                                            tempItem.KomusArt = valarr[1, j].ToString();
+                                        else
+                                            tempItem.KomusArt = "";
+                                        break;
+                                }
+                            if (tempItem.Category == "")
+                                tempItem.Category = tempItem.CorrectName();
+                            items.Add(tempItem);
+                        }
+                    }
                 }
+            }
+            finally
+            {
+                if (ecelbook != null)
+                {
+                    ecelbook.Close();
+                    Marshal.ReleaseComObject(ecelbook);
+                }
+                if (ExcelObj != null) ExcelObj.Quit();
+            }
+            for (int i=0; i < items.Count; i++)
+            {
+                if (!comboBox1.Items.Contains(items[i].Category))
+                    comboBox1.Items.Add(items[i].Category);
+                //comboBox2.Items.Add(items[i].Name);
             }
         }
 
@@ -53,77 +165,221 @@ namespace Kancelyar_zakaz
         private void button1_Click(object sender, EventArgs e)
         {
             int iSelected = comboBox1.SelectedIndex;
-            if (iSelected>=0)
-                if (!predmets[iSelected].isAdded)
-                {
-                    decimal N = numericUpDown1.Value;
-                    dataGridView1.Rows.Add(1);
-                    for (int i = 0; i < dataGridView1.Columns.Count; i++)
-                        switch (i)
-                        {
-                            case 0:
-                                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[i].Value = predmets[iSelected].Name;
-                                break;
-                            case 1:
-                                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[i].Value = predmets[iSelected].Art;
-                                break;
-                            case 2:
-                                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[i].Value = N.ToString();
-                                break;
-                        }
-                    Focus();
-                    if (dataGridView1.Rows.Count > 0)
-                        if (dataGridView1.SelectedCells.Count > 0)
-                            button2.Visible = true;
-                }
-                else
-                {
-                    //MessageBox
-                    MessageBox.Show("Элемент уже добавлен!");
-                }
-        }
 
-        List<NewPredmet> predmets = new List<NewPredmet>();
+            if (iSelected >= 0)
+            {
+
+                //if (!predmets[iSelected].isOrdered)//isAdded function to find this element
+
+                //{
+
+                decimal N = numericUpDown1.Value;
+
+                dataGridView1.Rows.Add(comboBox1.Text, comboBox2.Text, N.ToString(), textBox1.Text);
+
+                //dataGridView1.Rows.Add(1);
+
+                //for (int i = 1; i <= dataGridView1.Columns.Count; i++)
+
+                //    switch (i)
+
+                //    {
+
+                //        case 0:
+
+
+
+                //            dataGridView1.Rows[dataGridView1.Rows.Count].Cells[i].Value = predmets[iSelected].Name;
+
+                //            break;
+
+                //        case 1:
+
+                //            dataGridView1.Rows[dataGridView1.Rows.Count].Cells[i].Value = predmets[iSelected].Articul;
+
+                //            break;
+
+                //        case 2:
+
+                //            dataGridView1.Rows[dataGridView1.Rows.Count].Cells[i].Value = N.ToString();
+
+                //            break;
+
+                //    }
+
+                Focus();
+
+                if (dataGridView1.Rows.Count > 0)
+
+                    if (dataGridView1.SelectedCells.Count > 0)
+
+                        button2.Visible = true;
+
+            }
+
+            //}
+
+            //else
+
+            //{
+
+            //MessageBox
+
+            //MessageBox.Show("Элемент уже добавлен!");
+
+            //}
+
+
+
+            //set Form to default
+
+
+
+            label3.Visible = false;
+
+        }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            foreach (DataGridViewCell cell in dataGridView1.SelectedCells)
+            
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        List<Tuple<string, int>> indexList = new List<Tuple<string, int>>();
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.Enabled = true;
+            comboBox2.Items.Clear();
+            indexList.Clear();
+
+            for (int i = 0; i < items.Count; i++)
+                if (comboBox1.Items[comboBox1.SelectedIndex].ToString() == items[i].Category)
+                {
+                    comboBox2.Items.Add(items[i].CorrectName());
+                    Tuple<string, int> tuple = new Tuple<string, int>(items[i].CorrectName(), i);
+                    indexList.Add(tuple);
+                }
+
+            numericUpDown1.Enabled = false;
+            checkBox1.Enabled = false;
+            label3.Visible = false;
+            //toolStripStatusLabel1.Text = comboBox1.SelectedIndex.ToString();
+        }
+
+        Item selectedItem = new Item();
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            numericUpDown1.Enabled = true;
+            checkBox1.Enabled = true;
+            label3.Visible = true;
+
+            selectedItem = null;
+            string selectedName = comboBox2.Text;
+            for (int i=0; i<indexList.Count; i++)
+                if (indexList[i].Item1 == selectedName)
+                    selectedItem = items[indexList[i].Item2];
+
+            label3.Text = selectedItem.Unit;
+            textBox1.Text = selectedItem.KomusArt;
+        }
+
+        private void SelectionList(string sCategory, object sender)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Enabled = checkBox1.Checked;
+        }
+
+        //Unicum categorys of Item list ???Items globalen
+        private List<string> ListOfCategorys(List<Item> items)
+        {
+            List<string> list = new List<string>();
+            for (int i = 0; i < items.Count; i++)
+                if (!list.Contains(items[i].Category))
+                    list.Add(items[i].Category);
+            return list;
+        }
+        //Find element in list ???Items globalen
+        //private int FindElement(List<Item> items, string sVar, string ElementType, int FirstIndex = 0)
+        //{
+        //    int result = -1;
+        //    for (int i = 0; i < items.Count; i++)
+        //    {
+        //        switch (ElementType)
+        //        {
+        //            case "Category":
+
+        //        }
+        //    }
+        //}
+
+        private void checkBox1_CheckedChanged_1(object sender, EventArgs e)
+        {
+            textBox1.Enabled = !textBox1.Enabled;
+            if (textBox1.Enabled)
             {
-                dataGridView1.Rows.RemoveAt(cell.RowIndex);
+                textBox1.Focus();
+                textBox1.SelectAll();
             }
-            if (dataGridView1.SelectedCells.Count == 0)
-                button2.Visible = false;
         }
     }
-    [Serializable]
-    public class Predmet
+    //[Serializable]
+    public class Item
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
-        public string Art { get; set; }
-
-        public Predmet()
-        {
-        }
-        public Predmet(string id, string name, string art)
+        public string Code { get; set; }
+        public string Unit { get; set; }
+        public string KomusArt { get; set; }
+        public string Category { get; set; }
+        public Item(int id, string name, string code, string unit, string komus, string cat)
         {
             Id = id;
             Name = name;
-            Art = art;
+            Code = code;
+            Unit = unit;
+            KomusArt = komus;
+            Category = cat;
+        }
+        public Item()
+        {
+
+        }
+        public string CorrectName()
+        {
+            string outputtext = "";
+            for (int i = 0; i < Name.Length; i++)
+            {
+                if (Name[i] == '_')//switch if other uncorrect chars
+                    outputtext += " ";
+                else
+                    outputtext += Name[i].ToString();
+            }
+            return outputtext;
         }
     }
-    public class NewPredmet: Predmet
+
+    public class OrderItem : Item
     {
-        public string KomusArt { get; set; }
-        public bool isAdded { get; set; }
-        public NewPredmet()
+        public bool isOrdered { get; set; }
+        public string UserName { get; set; }
+        public string OrderTime { get; set; }
+        public int Count { get; set; }
+        public OrderItem(int id, string name, string code, string unit, string komus, string cat, string user, string time, int count)
+            : base(id, name, code, unit, komus, cat)
         {
-        }
-        public NewPredmet(string id, string name, string art, string komusArt="", bool added=false)
-            :base(id, name, art)
-        {
-            KomusArt = komusArt;
-            isAdded = added;
+            UserName = user;
+            OrderTime = time;
+            Count = count;
+            isOrdered = false;
         }
     }
+
 }
